a675509827dca8c20370b40e7dd364f7ba358cd5
oymdev04 <oymdev4@oym.com.py>
2018-10-30 15:39:50 -0300

    Pruebas JBS - bussines

---
+++ src/test/java/py/com/oym/test/bussines/DataLinkTest.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.com.oym.test.bussines;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import org.javabeanstack.data.IDataResult;
+import org.javabeanstack.data.IDataRow;
+import org.javabeanstack.data.IGenericDAORemote;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.javabeanstack.model.tables.Moneda;
+
+public class DataLinkTest extends TestClass {
+
+    private static IGenericDAORemote dao;
+
+    public DataLinkTest() {
+    }
+
+    @Test
+    public void monedas() throws Exception {
+        System.out.println("Test Moneda Persist");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+
+        Moneda moneda = new Moneda();
+
+        moneda.setCodigo("LIB");
+        moneda.setNombre("Libras Esterlinas");
+        moneda.setCambio(BigDecimal.valueOf(5L));
+        moneda.setIdempresa(Long.parseLong("41"));
+        moneda.setObservacion("Moneda para hacer transacciones con Inglaterra");
+        IDataResult dataResult = dataLink.persist(moneda);
+        Moneda monedaResult = dataResult.getRowUpdated();
+        System.out.println(dataResult.getErrorMsg());
+        assertTrue(dataResult.isSuccessFul());
+        dataLink.remove(monedaResult);
+    }
+
+    @Test
+    public void listas_monedas() throws Exception {
+        System.out.println("Lista de Monedas");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        String codigo_aux;
+        String nombre_aux;
+        BigDecimal cambios_aux;
+        List<Moneda> monedas = new ArrayList();
+        String[] v_codigo = {"AUS", "ARG", "RUS", "BOL"};
+        String[] v_nombre = {"AUSTRIA", "ARGENTINA", "RUSIA", "BOLIVIA"};
+        BigDecimal[] v_cambios = {BigDecimal.valueOf(3.5), BigDecimal.valueOf(1.2), BigDecimal.valueOf(6.5), BigDecimal.valueOf(0.5)};
+        for (int i = 0; i < 4; i++) {
+            codigo_aux = v_codigo[i];
+            nombre_aux = v_nombre[i];
+            cambios_aux = v_cambios[i];
+            Moneda moneda = new Moneda();
+            moneda.setCodigo(codigo_aux);
+            moneda.setNombre(nombre_aux);
+            moneda.setCambio(cambios_aux);
+            moneda.setIdempresa(41L);
+            monedas.add(moneda);
+        }
+        IDataResult dataResult = dataLink.persist(monedas);
+        List<IDataRow> monedasResult = dataResult.getRowsUpdated();
+        System.out.println(dataResult.getErrorMsg());
+        assertTrue(dataResult.isSuccessFul());
+        dataLink.remove(monedasResult);
+    }
+
+    @Test
+    public void Merge() throws Exception {
+        System.out.println("Merge");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        //INSERTAR
+        Moneda moneda = new Moneda();
+        moneda.setCodigo("LIB");
+        moneda.setNombre("Libras Esterlinas");
+        moneda.setCambio(BigDecimal.valueOf(5L));
+        moneda.setIdempresa(41L);
+        dataLink.persist(moneda);
+        //MERGE
+        moneda = dataLink.findByQuery("select o from Moneda o where codigo = 'LIB'", null);
+        moneda.setNombre("LIBRAS ESTERLINAS");
+        moneda.setCambio(BigDecimal.TEN);
+        IDataResult dataResult = dataLink.merge(moneda);
+        Moneda monedaResult = dataResult.getRowUpdated();
+        System.out.println(dataResult.getErrorMsg());
+        assertTrue(dataResult.isSuccessFul());
+        dataLink.remove(monedaResult);
+    }
+
+    @Test
+    public void monedasaction() throws Exception {
+        System.out.println("Test INSERTAR,MODIFICAR,BORRAR");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+
+        //insertar
+        Moneda moneda = new Moneda();
+        moneda.setCodigo("LIB");
+        moneda.setNombre("Libras Esterlinas");
+        moneda.setCambio(BigDecimal.valueOf(5L));
+        moneda.setIdempresa(Long.parseLong("41"));
+        moneda.setObservacion("Moneda para hacer transacciones con Inglaterra");
+        moneda.setAction(IDataRow.AGREGAR);
+        dataLink.update(moneda);
+
+        //actualizar
+        moneda = dataLink.findByQuery("select o from Moneda o where codigo = 'LIB'", null);
+        moneda.setNombre("LIBRAS ESTERLINAS");
+        moneda.setCambio(BigDecimal.TEN);
+        moneda.setAction(IDataRow.MODIFICAR);
+        dataLink.update(moneda);
+
+        //Borrar
+        moneda = dataLink.findByQuery("select o from Moneda o where codigo = 'LIB'", null);
+        moneda.setAction(IDataRow.BORRAR);
+        dataLink.update(moneda);
+
+    }
+
+    @Test
+    public void monedasactionlist() throws Exception {
+        System.out.println("Test INSERTAR LISTAS DE MONEDAS");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        //  AGREGAR
+        String codigo_aux;
+        String nombre_aux;
+        BigDecimal cambios_aux;
+        List<Moneda> monedas = new ArrayList();
+        String[] v_codigo = {"AUS", "ARG", "RUS", "BOL"};
+        String[] v_nombre = {"AUSTRIA", "ARGENTINA", "RUSIA", "BOLIVIA"};
+        BigDecimal[] v_cambios = {BigDecimal.valueOf(3.5), BigDecimal.valueOf(1.2), BigDecimal.valueOf(6.5), BigDecimal.valueOf(0.5)};
+        for (int i = 0; i < 4; i++) {
+            codigo_aux = v_codigo[i];
+            nombre_aux = v_nombre[i];
+            cambios_aux = v_cambios[i];
+            Moneda moneda = new Moneda();
+            moneda.setCodigo(codigo_aux);
+            moneda.setNombre(nombre_aux);
+            moneda.setCambio(cambios_aux);
+            moneda.setIdempresa(41L);
+            moneda.setAction(IDataRow.INSERT);
+            monedas.add(moneda);
+
+        }
+        dataLink.update(monedas);
+
+        //ELIMINAR
+        for (int i = 0; i < 4; i++) {
+            monedas.get(i).setAction(IDataRow.BORRAR);
+
+        }
+
+        dataLink.update(monedas);
+        IDataResult dataResult = dataLink.update(monedas);
+        System.out.println(dataResult.getErrorMsg());
+
+    }
+
+}
---
+++ src/test/java/py/com/oym/test/bussines/EjerciciosDataObjectTest.java
@@ -0,0 +1,329 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.com.oym.test.bussines;
+
+import java.math.BigDecimal;
+import javax.naming.NamingException;
+import org.javabeanstack.data.IGenericDAORemote;
+import org.javabeanstack.datactrl.DataObject;
+import org.javabeanstack.datactrl.IDataObject;
+import org.javabeanstack.exceptions.SessionError;
+import org.javabeanstack.model.tables.Moneda;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EjerciciosDataObjectTest extends TestClass {
+    
+    private static IGenericDAORemote dao;
+    
+    @Test
+    public void delete() throws Exception, NamingException, SessionError {
+        System.out.println("OPEN,INSERT,DELETE AND CLOSE");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        if (!moneda.find("codigo", "$")) {
+            moneda.insertRow();
+            moneda.setField("codigo", "$");
+            moneda.setField("nombre", "DOLARES");
+            moneda.setField("cambio", BigDecimal.ONE);
+            boolean result = moneda.update(false);
+            
+            assertTrue(result);
+            
+        } else {
+            System.out.println("No pasa nada");
+            
+        }
+        
+        if (moneda.find("codigo", "$")) {
+            moneda.deleteRow();
+            boolean result = moneda.update(false);
+            
+            assertTrue(result);
+            
+        }
+        moneda.close();
+    }
+    
+    @Test
+    public void testFilter() throws Exception, NamingException, SessionError {
+        System.out.println("SetFilter ");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.setFilter("codigo = 'EUR'");
+        moneda.requery();
+        
+        System.out.println("Set Filter--> " + moneda.getRow());
+    }
+    
+    @Test
+    public void testOrder() throws Exception, NamingException, SessionError {
+        System.out.println("Order");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.setOrder("idmoneda asc");
+        moneda.requery();
+        
+        System.out.println("Order --> " + moneda.getDataRows());
+    }
+    
+    @Test
+    public void testFisrtRow() throws Exception, NamingException, SessionError {
+        System.out.println("FirstRow");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.setFirstRow(0);
+        moneda.getFirstRow();
+        moneda.requery();
+        
+        System.out.println("Registros --> " + moneda.getDataRows());
+    }
+    
+    @Test
+    public void testMaxRow() throws Exception, NamingException, SessionError {
+        System.out.println("MaxRow");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.setMaxRows(2);
+        moneda.getMaxRows();
+        moneda.requery();
+        
+        System.out.println("Registros --> " + moneda.getDataRows());
+    }
+
+    //ver como se hace
+    @Test
+    public void testReadWrite() throws Exception, NamingException, SessionError {
+        System.out.println("ReadWrite");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.setReadWrite(true);
+        moneda.insertRow();
+        moneda.setField("codigo", "PER");
+        moneda.setField("nombre", "PERU");
+        
+        moneda.update(false);
+
+        //System.out.println("Registros --> " + moneda.getDataRows());
+    }
+    
+    @Test
+    public void testClaveForanea() throws Exception, NamingException, SessionError {
+        System.out.println("Clave Foranea");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        
+        assertFalse(moneda.isForeingKey("pais"));
+        
+    }
+    
+    @Test
+    public void testColumnaExiste() throws Exception, NamingException, SessionError {
+        System.out.println("Existencia de Columna");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        
+        assertTrue(moneda.isFieldExist("nombre"));
+        assertFalse(moneda.isFieldExist("nombres"));
+        
+    }
+    
+    @Test
+    public void testgetRow() throws Exception, NamingException, SessionError {
+        System.out.println("GET ROW");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.setFilter("idmoneda=245");
+        moneda.requery();
+        System.out.println("Valor -->" + moneda.getRow());
+        
+        assertNotNull(moneda.getRow());
+        
+    }
+    
+    @Test
+    public void testGetRows() throws Exception, NamingException, SessionError {
+        System.out.println("GET ROWS");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        
+        System.out.println("valores --> " + moneda.getDataRows());
+        assertNotNull(moneda.getDataRows());
+        
+    }
+    
+    @Test
+    public void testGetRecno() throws Exception, NamingException, SessionError {
+        System.out.println("GetRecno");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.find("codigo", "EUR");
+        int pos = 2;
+        assertEquals(pos, moneda.getRecno());
+        moneda.close();
+    }
+
+    //ver como se hace
+    @Test
+    public void testGoTo() throws Exception, NamingException, SessionError {
+        System.out.println("GoTo");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.goTo(2);
+        
+    }
+    
+    @Test
+    public void testMoveFirst() throws Exception, NamingException, SessionError {
+        System.out.println("MoveFirst");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.moveFirst();
+        String esperado = "GS ";
+        assertEquals(esperado, moneda.getField("codigo"));
+    }
+    
+    @Test
+    public void testMoveNext() throws Exception, NamingException, SessionError {
+        System.out.println("MoveNext");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.moveFirst();
+        moneda.moveNext();
+        String esperado = "USD";
+        assertEquals(esperado, moneda.getField("codigo"));
+    }
+    
+    @Test
+    public void testMovePrevius() throws Exception, NamingException, SessionError {
+        System.out.println("MovePrevius");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.find("codigo", "EUR");
+        moneda.movePrevious();
+        String esperado = "USD";
+        assertEquals(esperado, moneda.getField("codigo"));
+    }
+    
+    @Test
+    public void testMoveLast() throws Exception, NamingException, SessionError {
+        System.out.println("MoveLast");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.moveLast();
+        String esperado = "EUR";
+        assertEquals(esperado, moneda.getField("codigo"));
+    }
+    
+    @Test
+    public void testCantidad() throws Exception, NamingException, SessionError {
+        System.out.println("Cantidad de Valores");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        int canti = 3;
+        assertEquals(canti, moneda.getRowCount());
+        
+    }
+    
+    @Test
+    public void testGetError() throws Exception, NamingException, SessionError {
+        System.out.println("Get Error");
+        if (error != null) {
+            System.out.println(error);
+            return;
+        }
+        IDataObject<Moneda> moneda = new DataObject(Moneda.class, null, dataLink, null);
+        moneda.open();
+        moneda.insertRow();
+        assertNull(moneda.getErrorApp());        
+    }
+    
+    
+}
